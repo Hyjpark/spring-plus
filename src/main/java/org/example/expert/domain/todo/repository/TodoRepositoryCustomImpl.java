9140100bdc2c0d86bd799d2d4c640498a848d4eb
Hyjpark <hyej042@gmail.com>
2025-09-12 12:11:39 +0900

    feat: QueryDSL 적용하여 Todo 조회 시 User fetch join으로 N+1 문제 해결

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryCustomImpl.java
@@ -0,0 +1,23 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+
+@RequiredArgsConstructor
+public class TodoRepositoryCustomImpl implements TodoRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(todo)
+                .where(todo.id.eq(todoId))
+                .leftJoin(todo.user).fetchJoin()
+                .fetchOne());
+    }
+}

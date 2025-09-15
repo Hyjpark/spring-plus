71654288b26c6b33860f3ebd5d0aaa3329088cf7
Hyjpark <hyej042@gmail.com>
2025-09-15 20:32:00 +0900

    feat: QueryDSL 기반 Todo 검색 API 구현

---
+++ src/main/java/org/example/expert/domain/todo/dto/response/TodoSearchResponse.java
@@ -0,0 +1,19 @@
+package org.example.expert.domain.todo.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TodoSearchResponse {
+
+    private String title;
+    private int managerCount;
+    private int commentCount;
+
+    public TodoSearchResponse(String title, int managerCount, int commentCount) {
+        this.title = title;
+        this.managerCount = managerCount;
+        this.commentCount = commentCount;
+    }
+}
--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -91,6 +92,18 @@ public class TodoService {
                 new UserResponse(user.getId(), user.getEmail()),
                 todo.getCreatedAt(),
                 todo.getModifiedAt()
+        );
+    }
+
+    public Page<TodoSearchResponse> searchTodos(String title, String nickname, LocalDate startDate, LocalDate endDate, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        return todoRepository.searchTodos(
+                pageable,
+                title,
+                nickname,
+                startDate,
+                endDate
         );
     }
 }
